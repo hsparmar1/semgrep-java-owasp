3aa7f677d2fe191546019272349e7b86419ccdc3
hsparmar1 <hparmar@alephoneconsulting.com>
2022-02-28 20:45:56 -0500

    Initial Commit of Semgrep Java OWASP Demo

---
+++ src/main/java/com/alephone/semgrep/SemgrepApplication.java
@@ -0,0 +1,13 @@
+package com.alephone.semgrep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemgrepApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SemgrepApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alephone/semgrep/SemgrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alephone.semgrep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemgrepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
